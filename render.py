e79354705a1301cffcdc2b913ef878b35de65fe1
flaribbit <1149761294@qq.com>
2024-07-19 19:32:07 +0800

    update render.py

--- render.py
+++ render.py
@@ -1,5 +1,4 @@
 import hashlib
-import base64
 import re
 import os
 import tempfile
@@ -7,35 +6,59 @@ import subprocess
 
 
 def get_hash(input_string: str):
-    sha1_result = hashlib.sha1(input_string.encode()).digest()
-    base64_result = base64.b64encode(sha1_result, altchars=b"!$").decode()
-    return base64_result
+    sha1 = hashlib.sha1(input_string.encode()).digest()
+    return sha1[:10].hex()
+
+
+def get_files_md(rawname):
+    res = ""
+    for i in range(10):
+        filename = rawname.replace("{n}", str(i + 1))
+        if os.path.isfile(filename):
+            filename = filename.replace("docs/", "")
+            res += f"\n![]({filename})"
+    return res
 
 
 def process_file(filename: str):
+    def fun(m: re.Match):
+        block = m[0]
+        code = m[1]
+        print(f"=== Compiling:\n{code}")
+        # create a temporary file then use typst to compile it
+        outfilename = f"docs/generated/{get_hash(code)}_{{n}}.png"
+        # if exists then skip
+        if os.path.isfile(outfilename.replace("{n}", "1")):
+            print("=== Skipped")
+            return block + get_files_md(outfilename)
+        with tempfile.NamedTemporaryFile("w", encoding="utf-8") as f:
+            f.write("""#set page(height: 4cm, width: 6cm)
+#set text(font: ("Times New Roman", "Simsun"))
+""")
+            f.write(code)
+            f.flush()
+            result = subprocess.run(
+                ["typst", "compile", f.name, outfilename, "--font-path", "fonts"],
+                capture_output=True,
+                text=True,
+                encoding="utf-8",
+            )
+            stderr = result.stderr
+            if stderr:
+                print(stderr)
+        return block + get_files_md(outfilename)
+
     with open(filename, "r", encoding="utf-8") as f:
         content = f.read()
 
     pattern = re.compile(r"```typst\n(.*?)\n```", re.DOTALL)
-    matches = pattern.findall(content)
-    for m in matches:
-        # create a temporary file then use typst to compile it
-        outfilename = f"generated/{get_hash(m)}_{{n}}.png"
-        # if exists then skip
-        if os.path.isfile(outfilename):
-            print(f"Skipping {outfilename}")
-            continue
-        with tempfile.NamedTemporaryFile("w", delete=False, encoding="utf-8") as f:
-            f.write("#set page(height: 4cm, width: 6cm)\n")
-            f.write(m)
-        # print(f"Compiling {f.name} to {outfilename}")
-        # print("========== code:")
-        # print(m)
-        subprocess.run(["typst", "compile", f.name, outfilename])
+    content = pattern.sub(fun, content)
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(content)
 
 
 if __name__ == "__main__":
-    os.makedirs("generated", exist_ok=True)
+    os.makedirs("docs/generated", exist_ok=True)
     for root, dirs, files in os.walk("docs"):
         for file in files:
             if file.endswith(".md"):
