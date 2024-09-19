2c8a0c0af92f2316996736ca6078af715b4dbd12
flaribbit <1149761294@qq.com>
2024-09-19 10:05:39 +0800

    add dashboard

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -12,6 +12,13 @@ export default defineConfig({
       dark: 'github-dark'
     },
   },
+  head: [
+    ['script', {}, `(function(c,l,a,r,i,t,y){
+    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window, document, "clarity", "script", "o5fc884imi");`],
+  ],
   themeConfig: {
     editLink: {
       pattern: 'https://github.com/typst-doc-cn/guide/edit/master/docs/:path'
