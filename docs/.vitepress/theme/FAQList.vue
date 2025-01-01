547807d97ab00307fe5f948de1bd6572634d1251
flaribbit <1149761294@qq.com>
2025-01-01 09:45:27 +0800

    修复移动端问题目录不会换行, fix #25

--- docs/.vitepress/theme/FAQList.vue
+++ docs/.vitepress/theme/FAQList.vue
@@ -1,23 +1,12 @@
 <template>
   <div class="max-w-screen-lg w-full px-2 py-2 my-0 mx-auto">
     <div class="flex flex-wrap gap-1">
-      <a
-        :href="withBase(`FAQ#outline`)"
-        class="simple-tag"
-        style="text-decoration: none"
-        @click="currentTag = null"
-      >
+      <a :href="withBase(`FAQ#outline`)" class="simple-tag" style="text-decoration: none" @click="currentTag = null">
         <span>全部</span>
         <span class="pl-1"> {{ data.posts.length }} </span>
       </a>
-      <a
-        v-for="(tag, i) in tags"
-        :key="i"
-        :href="withBase(`FAQ?tag=${tag}#outline`)"
-        class="simple-tag"
-        style="text-decoration: none"
-        @click="currentTag = tag"
-      >
+      <a v-for="(tag, i) in tags" :key="i" :href="withBase(`FAQ?tag=${tag}#outline`)" class="simple-tag"
+        style="text-decoration: none" @click="currentTag = tag">
         <span>{{ tag }}</span>
         <span class="pl-1"> {{ data.tagMap[tag].length }} </span>
       </a>
@@ -25,8 +14,7 @@
     <div class="my-2 text-2xl">{{ currentTag }}</div>
     <ul>
       <li v-for="(article, index) in postList" :key="index">
-        <a :href="withBase(article.url)" class="overflow-hidden whitespace-nowrap text-ellipsis"
-          style="text-decoration: none">
+        <a :href="withBase(article.url)" style="text-decoration: none">
           {{ article.title }}
         </a>
       </li>
