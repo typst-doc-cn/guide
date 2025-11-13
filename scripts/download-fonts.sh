4776b5eacb304a0cce6585e81b41c75559a0b49c
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2025-11-13 10:11:41 +0800

    ci: add CMSY10_fix.otf

---
+++ scripts/download-fonts.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+# Downloading fonts is time consuming. Please cache them if possible.
+
+mkdir -p fonts
+cd fonts
+
+curl -OL https://github.com/typst-doc-cn/guide/releases/download/files/fonts.7z
+7z x fonts.7z
+
+curl -OL https://github.com/matplotlib/matplotlib/raw/be68dfecf9d26ac1a8e1e30a0de6171ecf174cd5/lib/matplotlib/mpl-data/fonts/ttf/cmsy10.ttf
+
+curl -OL https://github.com/typst-doc-cn/guide/releases/download/files/CMSY10_fix.otf
+
+cd -
