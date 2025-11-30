0d4f60eb155298947a7b39d583e694a639c56f5a
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2025-11-30 21:03:45 +0800

    fix: Circumvent a Netlify quirk

--- docs/.vitepress/theme/SeeAlso.vue
+++ docs/.vitepress/theme/SeeAlso.vue
@@ -7,9 +7,20 @@ import { data, type Link, type RelativePath } from './see_also.data';
 const route = useRoute();
 const { site } = useData();
 
+/**
+ * Fix the path for Netlify.
+ *
+ * Netlify 部署 URL 比较奇怪：
+ * - 若先访问 /FAQ.html，再单击跳转到 /FAQ/bib-etal-lang.html，则`path === "/FAQ/bib-etal-lang.html"`，正常。
+ * - 若直接访问 /FAQ/bib-etal-lang.html，则`path === "/faq/bib-etal-lang.html"`，变成小写。
+ */
+function fixNetlify(path: RelativePath): RelativePath {
+  return path.replace('/faq/', '/FAQ/') as RelativePath;
+}
+
 const links = computed<Link[] | null>(() => {
   const path: RelativePath = `/${removePrefix(route.path, site.value.base)}`;
-  return data.linksIndex[path] ?? null;
+  return data.linksIndex[path] ?? data.linksIndex[fixNetlify(path)] ?? null;
 });
 </script>
 <template>
